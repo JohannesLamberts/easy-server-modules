87851786464c2db0bf6f27eb7559a5d91b55102e
Johannes Lamberts <mail@j-lamberts.de>
2018-03-10 19:17:47 +0100

    feat(websocket): simplify interface - rename WebsocketConnection -> WebsocketConnectorBase - hide _socket and only expose on() and emit() - support multiple WebsocketConnectors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { EHttpState }          from './modules/express/httpState';
-export { WebsocketConnection } from './modules/websocket/connection';
-export *                       from './serverEnvironment';
-export { ApiSegment }          from './modules/api/apiSegment';
+export { EHttpState }             from './modules/express/httpState';
+export { WebsocketConnectorBase } from './modules/websocket/connection';
+export *                          from './serverEnvironment';
+export { ApiSegment }             from './modules/api/apiSegment';
